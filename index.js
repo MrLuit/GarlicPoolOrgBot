7b9fe2a0b2b8054e2e034f1aede122cd0b75d79b
Bart Smienk <bart@coolmail.nl>
2018-01-28 02:33:24 +0100

    Stop the chaos
    Should fix (most) things

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ Bot.prototype.updateData = async function () {
     const garlic_value = snekfetch.get('https://api.coinmarketcap.com/v1/ticker/garlicoin/');
     const poolstats = snekfetch.get(`https://garlicpool.org/index.php?page=api&action=getpoolstatus&api_key=${config.garlicpool_api_key}`);
     const {body} = await snekfetch.get(`https://garlicpool.org/index.php?page=api&action=getdashboarddata&api_key=${config.garlicpool_api_key}`);
-    this.pool_data = JSON.parse(body.toString()).getdashboarddata.data;
+	this.pool_data = JSON.parse(body.toString()).getdashboarddata.data;
     let hashrate = (this.pool_data.raw.pool.hashrate / 1000).toFixed(2);
     console.log(`Hashrate: ${hashrate} MH/s`);
     client.user.setActivity(`${hashrate} MH/s`);
@@ -66,8 +66,8 @@ Bot.prototype.updateData = async function () {
         console.log(`New block mined: ${block.id}`);
     });
     const response_stats = await poolstats;
-    this.pool_stats = JSON.parse(response_stats.body.toString()).getpoolstatus.data;
-    const {text} = await snekfetch.get(`https://explorer.grlc-bakery.fun/api/getblockhash?index=${this.pool_stats.currentnetworkblock}`);
+    this.pool_stats = response_stats.body;
+    const { text } = await snekfetch.get(`https://explorer.grlc-bakery.fun/api/getblockhash?index=${this.pool_stats.currentnetworkblock}`);
     this.pool_stats.currentBlockHash = text;
 
     this.garlic_data = (await garlic_value).body[0];
